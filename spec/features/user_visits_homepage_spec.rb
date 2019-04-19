e312f0ac05a56e4d92952b1554f751ca7378799f
tahbristol <tahbristol@gmail.com>
2019-04-18 23:03:00 -0400

    fixed test, added capybara

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "User visits homepage" do
-  senario "successfully" do
+  scenario "successfully" do
     visit root_path
     
     expect(page).to have_css 'h1', text: 'Todos'
