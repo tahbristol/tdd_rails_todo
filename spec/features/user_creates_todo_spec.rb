d021e3a8773da54d2a63cfd459e2b7a49dabf804
tahbristol <tahbristol@gmail.com>
2019-04-19 23:33:45 -0400

    allow user to create todo test, ruby 2.5.1

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+    
+    click_on "Add new todo"
+    fill_in "todo[title]", with: "This is a todo"
+    click_on "Create Todo"
+    
+    expect(page).to have_css('.todos li', text: 'This is a todo')
+  end
+end
